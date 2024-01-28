0d0186189836e38bbb05f48bbe8ffa722d64ccad
J0aoCunha <jvjunger20123@gmail.com>
2024-01-28 17:14:40 -0300

    chore: configurando o knex

--- src/serve.ts
+++ src/serve.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './dataBase'
 
 const app = fastify()
+
+app.get('/', async () => {
+  const test = await knex('sqlite_schema').select('*')
+
+  return test
+})
 
 app
   .listen({
